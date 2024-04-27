c2208cd5252189ef1f71fae848bb80e17258d550
omar ayman <oaayoub0101@gmail.com>
2024-04-27 23:26:19 +0300

    added refine query function

--- src/main/java/org/example/demo/GeminiApiRequest.java
+++ src/main/java/org/example/demo/GeminiApiRequest.java
@@ -58,4 +58,55 @@ public class GeminiApiRequest {
         }
         return apiKey;
     }
+
+
+    public static String refineContent(String queryText) {
+        OkHttpClient client = new OkHttpClient();
+
+        // Replace "YOUR_API_KEY" with your actual Google API key
+        String apiKey = "AIzaSyAXdsMLYA8_g95nBiPuQ35-wX4TSXbGea4";
+        String url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + apiKey;
+        String queryHeader = "fix this sparql query and return it without any extra characters: ";
+        String query = queryHeader + queryText;
+        // JSON request body
+        String jsonReq = "{\"contents\":[{\"parts\":[{\"text\":\"" + query + "\"}]}]}";
+
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(jsonReq, mediaType);
+        // Create a Gson instance
+
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        // Execute the request and handle the response
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                Gson gson = new Gson();
+                JsonObject jsonObject = gson.fromJson(response.body().string(), JsonObject.class);
+                System.out.println(jsonObject);
+
+                // Extract the "candidates" array directly
+                JsonArray candidatesArray = jsonObject.getAsJsonArray("candidates");
+                System.out.println(candidatesArray);
+
+                // Extract the "text" field from the first element of the "parts" array within the "content" object
+                String text = candidatesArray.get(0).getAsJsonObject().getAsJsonObject("content").getAsJsonArray("parts").get(0).getAsJsonObject().get("text").getAsString();
+                System.out.println("OMAR ::: " + text);
+                text = text.replaceAll("```", "");
+                text = text.replaceAll("sparql", "");
+
+                return text;
+            } else {
+                System.out.println("Request failed: " + response);
+                return response.body().string();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return apiKey;
+    }
 }
